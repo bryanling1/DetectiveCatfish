5c8b5a2fc85d08e4ff8d9292da66aa2c117403e5
b3ling <b3ling@edu.uwaterloo.ca>
2020-04-04 18:19:52 -0400

    Added mask to device and button tabs working

---
+++ Assets/Scripts/Tab.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tab : MonoBehaviour
+{
+    [SerializeField] GameObject tab;
+
+    //Cached references
+    Tabs tabs;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        tabs = FindObjectOfType<Tabs>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void activateTab(){
+        tabs.hideAllTabs();
+        tab.SetActive(true);
+    }
+}
