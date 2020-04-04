6a3e501cac215c6081c4ad06ecdc97815affecfc
b3ling <b3ling@uwaterloo.ca>
2020-04-04 17:49:31 -0400

    Imported AR device assets

---
+++ Assets/Scripts/Tabs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tabs : MonoBehaviour
+{
+    [SerializeField] List<GameObject> tabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void hideAllTabs(){
+        for(int i=0; i<tabs.Count; i ++){
+            tabs[i].SetActive(false);
+        }
+    }
+}
